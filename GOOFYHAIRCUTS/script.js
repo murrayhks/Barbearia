3ebc6604daa66ed08b63168155cee4f864a318c6
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-02-12 21:09:51 -0300

    script.js

--- GOOFYHAIRCUTS/script.js
+++ GOOFYHAIRCUTS/script.js
@@ -1,9 +1,7 @@
-// Splash Screen
 setTimeout(() => {
     document.getElementById('splash-screen').style.display = 'none';
 }, 5000);
 
-// Menu Hambúrguer
 function toggleMenu() {
     const nav = document.getElementById('navbar');
     nav.classList.toggle('active');
